9d9b203a7fe0a60b86b9b4fa0d2063cbef4869d3
Aryan Patil <aryan.apatil@gmail.com>
2024-01-22 17:15:00 -0500

    mm

--- Frontend-Native/Components/Places.js
+++ Frontend-Native/Components/Places.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import PlacesAutocomplete from "expo-google-places-autocomplete";
 
 const GooglePlacesInput = ({ setSearchLocation }) => {
 
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("GET", "https://google.com", false);
+  
+
+
     return (
-        <GooglePlacesAutocomplete
-            placeholder='Search'
-            // minLength={2}
-            onFail={(err) => console.error(err)}
-            onPress={(data, details = null) => {
-            //     // 'details' is provided when fetchDetails = true
-            
-            if(details){
-                console.log(details.geometry.location);
-                setSearchLocation(details.geometry.location);
-                // const { geometry } = details;
-                // console.log(location);
-            }
-            }}
-            query={{
-                key: 'AIzaSyDtV9_n1eiNhjKuB5hr8iBeKEIg-Z_FTck',
-                language: 'en',
-            }}
-            fetchDetails={true}
-            // keyboardShouldPersistTaps="handled"
-            // debounce={300}
-        />
+
+        
+            <GooglePlacesAutocomplete
+                placeholder='Search'
+                onFail={(err) => console.error(err)}
+                onPress={
+                    (data, details = null) => {
+
+                        if (details) {
+                            // console.log(details.geometry.location);
+                            setSearchLocation(details.geometry.location);
+
+                        }
+                    }}
+
+                query={{
+                    key: 'AIzaSyDtV9_n1eiNhjKuB5hr8iBeKEIg-Z_FTck',
+                    language: 'en',
+                }}
+            // fetchDetails={true}
+
+            />
+        
+
+       
     );
 
     
