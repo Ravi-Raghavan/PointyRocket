e4162179dcb07f5798aa0905a24979dbcbccbe81
Ravi Raghavan <ravi0308raghavan@gmail.com>
2024-01-29 16:00:45 -0500

    Finished adding endpoint to backend

--- Backend/flask_backend.py
+++ Backend/flask_backend.py
@@ -3,9 +3,18 @@ from flask_cors import CORS
 import json
 import redis
 
+from pymongo import MongoClient
+
+
 #Flask Server Information
 app = Flask(__name__)
 CORS(app, origin = 'http://localhost:8000', resources={r"/*": {"origins": "*"}})
+
+#MongoDB Information
+uri = "mongodb+srv://raviraghavan:gnGgV5YnXLFrYXJ7@capstone.fj6fsut.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
 
 #Redis Data Store Information
 r = redis.Redis(
@@ -39,6 +48,41 @@ def submit_path():
         r.rpush("path_list", key)
         
         return 'Data Successfully Submitted'
+
+# Save button to tell application to save Redis Data to MongoDB
+@app.route("/save_data", methods = ["POST"])
+def save_data():
+    if request.method == 'POST':
+        # Send a ping to confirm a successful connection
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+    
+        # Get the current length of the list containing documents
+        most_recent_path = r.get("path_counter")
+        if most_recent_path is not None:
+            most_recent_path = int(most_recent_path)
+        else:
+            most_recent_path = 0
+
+        print("Most Recent Path Index:", most_recent_path)
+
+        #Fetch Path Data of Most Recent Path
+        path_data = r.lrange(f"path:{most_recent_path}", 0, -1)[0]
+        path_data = json.loads(path_data)
+
+        #Get MongoDB Collection
+        db = client["Saved_Data"]
+        collection = db["Saved_Paths"]
+
+        #Store Data to MongoDB
+        result = collection.insert_one(path_data)
+        print(f"Inserted Recent Path with ID: {result.inserted_id}")
+        
+        return 'Data Saved to MongoDB'
+        
 
 #Run Flask Application
 if __name__ == "__main__":
