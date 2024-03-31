9c61cdf100ac743fab80ce708f250ab6f85c97b1
Ravi Raghavan <ravi0308raghavan@gmail.com>
2024-03-31 19:19:26 -0400

    Added traveling salesman endpoints

---
+++ Drone/get_traveling_salesman_path_test.py
@@ -0,0 +1,23 @@
+import websocket
+import requests
+import json
+
+#URL endpoint of backend server
+backend_url = "https://factual-moved-snapper.ngrok-free.app/get_traveling_salesman_path"
+
+#Get Next Point on Traveling Salesman Path From the Backend Server
+# Sending the GET request
+response = requests.get(backend_url)
+
+# Checking the response
+if response.status_code == 200:
+    print("GET request was successful!")
+    print("Response:", response.text)
+    
+    data = response.json()
+    if data != None:
+        print("Data: ", data)
+        print("Type of Data: ", type(data))
+else:
+    print("GET request failed with status code:", response.status_code)
+
